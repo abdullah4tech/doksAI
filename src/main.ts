2430d89d3429637388e3d02d074ea1f7e51243d5
Abdullah Mustapha <a41622100@gmail.com>
2025-11-10 01:32:18 +0000

    feat: init Motion lib

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import "./styles/main.css"
+import './styles/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from "pinia"
+import { createPinia } from 'pinia'
+import { MotionPlugin } from '@vueuse/motion'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(MotionPlugin)
 
 app.mount('#app')
